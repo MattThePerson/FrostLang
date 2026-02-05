81f80c45861fc522a851afe0f4b2955eab8fd426
Matt Stirling <stirlingmattmarkus@gmail.com>
2026-02-05 23:38:43 +0200

    progres: improved lexing of strings

--- bootstrap/Lexer.cpp
+++ bootstrap/Lexer.cpp
@@ -1,4 +1,5 @@
 #include "Lexer.hpp"
+
 #include <iostream>
 #include <unordered_set>
 #include <vector>
@@ -10,21 +11,49 @@
 std::vector<std::string> tokenize(std::istream &stream) {
 
     std::string ignore_chars = "\n\0\r\t";
-    std::string single_token_chars = "(){}=*[];,";
-    std::unordered_set<std::string> type_tokens = {"int", "float", "string"};
+    std::string single_token_chars = "<>(){}=[];:,.\"+-/*";
+    std::unordered_set<std::string> type_tokens = {"uind", "ind"};
 
     std::vector<std::string> token_vect;
     std::string buffer;
     char ch;
 
     // 'value' token, eg. occuring between '=' and ';'
-    bool FLAG_reading_value = false;
+    // bool reading_value = false;
+    bool quotes_open = false;
+    bool special_char_flag = false;
 
     // read stream one char at a time
     while (stream.get(ch)) {
 
-        // SPECIAL CASE: all spaces are ignored unless ocurring between type and
-        // variable name, where they indicate token completion
+        // detect quotes state change
+        if (ch == '"' && !special_char_flag) {
+            if (quotes_open) { // they were open
+                token_vect.push_back(buffer);
+                buffer.clear();
+            }
+            quotes_open = !quotes_open;
+            token_vect.push_back(std::string(1, '"'));
+            continue;
+        }
+
+        // handle strings
+        if (quotes_open) {
+            if (ch == '\\' && !special_char_flag) {
+                special_char_flag = true;
+                continue;
+            } else if (special_char_flag) {
+                special_char_flag = false;
+                if (ch != '"' && ch != '\\') {
+                    buffer.push_back('\\');
+                }
+            }
+            buffer.push_back(ch);
+            continue;
+        }
+
+        // SPECIAL CASE: all spaces are ignored unless ocurring between type
+        // and variable name, where they indicate token completion
         if (ch == ' ') {
             if (type_tokens.count(buffer) || buffer == "return") {
                 token_vect.push_back(buffer);
@@ -53,6 +82,12 @@ std::vector<std::string> tokenize(std::istream &stream) {
     // add final token
     if (!buffer.empty()) {
         token_vect.push_back(buffer);
+    }
+
+    // exception_handling
+    if (quotes_open) {
+        std::cerr << "LEXING_ERROR: quotes not closed\n";
+        exit(1);
     }
 
     return token_vect;
