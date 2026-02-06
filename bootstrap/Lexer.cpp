5f60bbc5f274a71a4a00632c61e78ba75d7792c6
Matt Stirling <stirlingmattmarkus@gmail.com>
2026-02-06 09:57:11 +0200

    minor clean: lexer, added more types

--- bootstrap/Lexer.cpp
+++ bootstrap/Lexer.cpp
@@ -4,17 +4,20 @@
 #include <unordered_set>
 #include <vector>
 
+std::string ESCAPE_SEQUENCES{'\0', '\n', '\r', '\t', '\b', '\f', '\v', '\a'};
+std::string ESCAPE_SEQUENCE_CHARS = "0nrtbfva";
+
+std::string SINGLE_CHAR_TOKENS = "<>(){}=[];:,.\"+-/*";
+
+std::unordered_set<std::string> TYPE_TOKENS = {
+    "ind",   "in8", "in16", "in32", "in64", "uind", "uin8", "uin16", "uin32",
+    "uin64", "fld", "fl8",  "fl16", "fl32", "fl64", "vd",   "chr"};
+
 /*
  * tokenize_stream - Apply lexing to stream of code, extracting and returning a
  * list of tokens.
  */
 std::vector<std::string> tokenize(std::istream &stream) {
-
-    std::string escape_sequences{'\0', '\n', '\r', '\t',
-                                 '\b', '\f', '\v', '\a'};
-    std::string escape_sequence_chars = "0nrtbfva";
-    std::string single_token_chars = "<>(){}=[];:,.\"+-/*";
-    std::unordered_set<std::string> type_tokens = {"uind", "ind"};
 
     std::vector<std::string> token_vect;
     std::string buffer;
@@ -49,7 +52,7 @@ std::vector<std::string> tokenize(std::istream &stream) {
                 if (ch == '"' || ch == '\\' || ch == '\'' || ch == '?') {
                     buffer.push_back(ch);
                 }
-                if (escape_sequence_chars.find(ch) != std::string::npos) {
+                if (ESCAPE_SEQUENCE_CHARS.find(ch) != std::string::npos) {
                     token_vect.push_back(buffer);
                     buffer.clear();
                     token_vect.push_back(std::string{'\\', ch});
@@ -63,7 +66,7 @@ std::vector<std::string> tokenize(std::istream &stream) {
         // SPECIAL CASE: all spaces are ignored unless ocurring between type
         // and variable name, where they indicate token completion
         if (ch == ' ') {
-            if (type_tokens.count(buffer) || buffer == "return") {
+            if (TYPE_TOKENS.count(buffer) || buffer == "return") {
                 token_vect.push_back(buffer);
                 buffer.clear();
             }
@@ -71,12 +74,12 @@ std::vector<std::string> tokenize(std::istream &stream) {
         }
 
         // ignore char
-        if (escape_sequences.find(ch) != std::string::npos) {
+        if (ESCAPE_SEQUENCES.find(ch) != std::string::npos) {
             continue;
         }
 
         // single token char
-        if (single_token_chars.find(ch) != std::string::npos) {
+        if (SINGLE_CHAR_TOKENS.find(ch) != std::string::npos) {
             if (!buffer.empty()) {
                 token_vect.push_back(buffer);
                 buffer.clear();
